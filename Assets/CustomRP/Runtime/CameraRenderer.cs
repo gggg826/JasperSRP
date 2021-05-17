ede65751aa7a57b04b0abac01d96b47582d75de2
BingLau <gggg826@users.noreply.github.com>
2021-05-17 23:56:47 +0800

    update 1 lesson

---
+++ Assets/CustomRP/Runtime/CameraRenderer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class CameraRenderer : MonoBehaviour
+{
+    private ScriptableRenderContext m_Context;
+    private Camera m_Camera;
+
+    public void Render(ScriptableRenderContext context, Camera camera)
+    {
+        m_Context = context;
+        m_Camera = camera;    
+    }
+}
---
+++ Assets/CustomRP/Runtime/CustomRenderPipLine.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class CustomRenderPipLine : RenderPipeline
+{
+    protected override void Render(ScriptableRenderContext context, Camera[] cameras)
+    {
+
+    }
+}
